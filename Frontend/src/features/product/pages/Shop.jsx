7a22a602bfe47b7258d2f8e2e1d54d7afe62a19b
Sonu Kumar <kumarsk002005@gmail.com>
2026-07-11 12:11:48 +0530

    style: format code for improved readability in Shop component

--- Frontend/src/features/product/pages/Shop.jsx
+++ Frontend/src/features/product/pages/Shop.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { useProduct } from '../hooks/use.product'
 import { useEffect } from 'react'
 const Shop = () => {
- const {handleGetAllProducts} = useProduct()
- useEffect(()=>{
+  const { handleGetAllProducts } = useProduct()
+  useEffect(() => {
     handleGetAllProducts()
- },[])
+  }, [])
   return (
     <div>Shop</div>
   )
